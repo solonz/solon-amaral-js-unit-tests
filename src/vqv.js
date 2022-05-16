570f336a86b7b4448193e0f5b879b97d60b54a00
Solon <amaral.solon@hotmail.com>
2022-05-16 12:56:09 -0300

    Job 3 - Done

--- src/vqv.js
+++ src/vqv.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
 
+const numbers = require('./numbers');
+
 /*
   Use template literals para escrever uma função que,
   recebe seu nome e sua idade e retorna o parágrafo descrito abaixo.
@@ -16,6 +18,14 @@
       #VQV!'
 */
 
-const vqv = () => {};
-
+const vqv = (nome, idade) => {
+  const frase = `Oi, meu nome é ${nome}!\n`
+  + `Tenho ${idade} anos,\n`
+  + 'trabalho na Trybe e mando muito em programação!\n'
+  + '#VQV!';
+  if (nome === '' || typeof idade !== 'number') {
+    return undefined;
+  } return frase;
+  };
+console.log(vqv(30, 27));
 module.exports = vqv;
